3f1ffae7a704392f71391fa1ebea62c50996c57c
ithek <kev.thek@gmail.com>
2014-03-25 20:40:08 +0100

    Pousse caisse

---
+++ Assets/EnableLS.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableLS : MonoBehaviour {
+	public Transform target;
+	public Transform patte;
+
+	private bool ena = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyUp("p")) {
+			if(ena) {
+				gameObject.animation.enabled = false;
+				gameObject.GetComponent<LegAnimator>().enabled = false;
+				gameObject.GetComponent<NormalCharacterMotor>().enabled = false;
+				gameObject.GetComponent<PlatformCharacterController>().enabled = false;
+
+				target.GetComponent<TargetController>().enabled = true;
+				patte.GetComponent<CCD3d>().enabled = true;
+				ena = false;
+			}
+			else {
+				gameObject.animation.enabled = true;
+				gameObject.GetComponent<LegAnimator>().enabled = true;
+				gameObject.GetComponent<NormalCharacterMotor>().enabled = true;
+				gameObject.GetComponent<PlatformCharacterController>().enabled = true;
+				
+				target.GetComponent<TargetController>().enabled = false;
+				patte.GetComponent<CCD3d>().enabled = false;
+				ena = true;
+			}
+		}
+	}
+}
--- Assets/StandUp.cs
+++ Assets/StandUp.cs
@@ -1,23 +1,22 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public class CCD3d : MonoBehaviour {
+public class StandUp : MonoBehaviour {
 	#region script_parameters	
 	// the target we want the end-effector to reach
 	// In our case the cube
-	public Transform target;
 	public Transform armStart;
 	#endregion script_parameters
-		
+	
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// in each frame we make one iteration
-		CCDStep3D(gameObject.transform.parent, gameObject.transform, target);
+		CCDStep3D(gameObject.transform.parent, gameObject.transform);
+
 	}
 	
 	/// \brief method for computing an angle value between two 3d vectors using
@@ -25,7 +24,7 @@ public class CCD3d : MonoBehaviour {
 	/// \return the value of the angle existing between a and b.
 	private float ComputeAngle3D(Vector3 a, Vector3 b)
 	{
-        float alpha = 0;
+		float alpha = 0;
 		float prodScal = Vector3.Dot (a, b);
 		alpha = Vector3.Angle(a,b);
 		return alpha;
@@ -39,17 +38,17 @@ public class CCD3d : MonoBehaviour {
 	/// \param joint the current joint that will be rotated towards the target.
 	/// \param effector the end effector transform.
 	/// \param target the transform containing the position we want the end-effector to reach.
-	private void CCDStep3D(Transform joint, Transform effector, Transform target)
+	private void CCDStep3D(Transform joint, Transform effector)
 	{
 		Vector3 qipe = new Vector3 (effector.position.x - joint.position.x, effector.position.y - joint.position.y, effector.position.z - joint.position.z);
-		Vector3 qipt = new Vector3 (target.position.x - joint.position.x, target.position.y - joint.position.y, target.position.z - joint.position.z);
+		Vector3 qipt = new Vector3 (armStart.position.x - joint.position.x, armStart.position.y + 500 - joint.position.y, armStart.position.z - joint.position.z);
 		float alpha = ComputeAngle3D (qipe, qipt);
 		Vector3 axe = Vector3.Cross (qipe, qipt);
 		joint.Rotate (axe, alpha, Space.World);
-
+		
 		if(joint.parent != null && joint != armStart)
 		{
-			CCDStep3D(joint.parent, effector, target);
+			CCDStep3D(joint.parent, effector);
 		}
 	}
 }
