e53e6eced42effff9eff5cd165520e316c5c3d5e
Paul Rivière <rivierepaul@hotmail.fr>
2014-04-16 17:43:57 +0200

    Première révisions scripts mur + MAJ dossiers

---
+++ Assets/MurDoigts.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MurDoigts : MonoBehaviour {
+
+	public Transform[] TableauDePrises;
+	public Transform DebutMembre;
+	public Transform PriseDeDebut;
+	public int epsilon; //Correspond à la distance limite entre les doigts et la prise
+
+	Transform priseEnCours;
+
+
+	// Use this for initialization
+	void Start () {
+		priseEnCours = PriseDeDebut;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Vérification de la distance 
+		if (Vector3.Distance (transform.position, priseEnCours.position) > epsilon) {
+			Transform priseProche = priseEnCours;
+			//Recherche de la prise la plus proche 
+			for (int i =0;i<TableauDePrises.Length;i++) {
+				if (Vector3.Distance(transform.position,TableauDePrises[i].position) <  Vector3.Distance(transform.position,priseProche.position)) 
+				    priseProche = TableauDePrises[i];
+			}
+			priseEnCours=priseProche;
+		}
+
+	    //Ensuite, il faut changer la target pour le script CCD
+		CCD3d test = (CCD3d)(transform.GetComponent ("CCD3d"));
+	    test.target=priseEnCours;
+	}
+
+	//TODO : méthode getPrise
+}
---
+++ Assets/MurRoot.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MurRoot : MonoBehaviour {
+
+	public Transform mur;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Gestion du déplacement du root
+
+		//Vecteur mouvement
+		Vector3 move = new Vector3(0,0,0);
+
+		Vector3 val;
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			//val = mur.up; val.Scale(0.1);
+			move = move + (0.1f * mur.up);
+		}
+		/*if (Input.GetKey (KeyCode.DownArrow)) 
+						move = move - 0.1 * mur.up;
+
+		if (Input.GetKey (KeyCode.LeftArrow))
+						move = move - 0.1 * mur.forward;
+		if (Input.GetKey (KeyCode.RightArrow))
+			move = move + 0.1 * mur.forward;*/
+
+		transform.position += move;
+
+		//Le passage à une prise ou à une autre est faite par les scripts MurDoigts
+	}
+}
