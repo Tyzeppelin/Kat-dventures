137b12a78b9e240c210f316141e4a1c5faa3477a
Tyzeppelin <boschet.francois@gmail.com>
2014-04-21 15:39:56 +0200

    Start fixed

--- Assets/Start/Name_Start.cs
+++ Assets/Start/Name_Start.cs
@@ -3,11 +3,15 @@ using System.Collections;
 
 public class Name_Start : MonoBehaviour {
 
+	private int screenWidth;
+
 	// Use this for initialization
 	void Start () {
-		screenRes = Screen.currentResolution;
-		Vector3 pos = new Vector3(Mathf.Atan(screenRes.width)/(2*Mathf.PI),Mathf.Atan(screenRes.height)/(2*Mathf.PI),0);
-		this.transform.position = pos;
+		screenWidth = Screen.width;
+		// Vector3 pos = new Vector3(Mathf.Atan(screenRes.width)/(4*Mathf.PI),Mathf.Atan(screenRes.height)/(2*Mathf.PI),0);
+		// this.transform.position = pos;
+		print(screenWidth);
+		this.guiText.fontSize = (int)(screenWidth/13.6);
 	}
 	
 	// Update is called once per frame
