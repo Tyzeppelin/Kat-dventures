e7deb183653a2246c3d8098fc573e62d9f26cd9a
ithek <kev.thek@gmail.com>
2014-05-11 12:00:52 +0200

    temp

--- Assets/pousse/StandUp.cs
+++ Assets/pousse/StandUp.cs
@@ -6,18 +6,23 @@ public class StandUp : MonoBehaviour {
 	// the target we want the end-effector to reach
 	// In our case the cube
 	public Transform start;
+	public Transform[] squelette = new Transform[5];
 	#endregion script_parameters
-	//Tete du nouveau squelette
-	public Transform[] i = new Transform[5];
-	private int a = 1;
+	private int maxY = 500;
+	private int maxZ = 200;
+	private int y = 1;
+	private int z = 1;
+
 	// Use this for initialization
 	void Start () {
-
+		int i = 1;
+		Vector3 target = new Vector3 (start.position.x, start.position.y + 1, start.position.z + 1);
+		CCDStep3D(squelette[1], squelette[0], target, i);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		CCDStep3D(i[1], i[0]);
+
 	}
 
 	/// \brief method for computing an angle value between two 3d vectors using
@@ -39,21 +44,25 @@ public class StandUp : MonoBehaviour {
 	/// \param joint the current joint that will be rotated towards the target.
 	/// \param effector the end effector transform.
 	/// \param target the transform containing the position we want the end-effector to reach.
-	private void CCDStep3D(Transform joint, Transform effector)
+	private void CCDStep3D(Transform joint, Transform effector, Vector3 target, int i)
 	{
-		if (a < 5) 
+		Vector3 qipe = effector.position - joint.position;
+		Vector3 qipt = new Vector3 (start.position.x - joint.position.x, start.position.y - joint.position.y, start.position.z - joint.position.z);
+		//Vector3 qipt = start.position - target; 
+		float alpha = ComputeAngle3D (qipe, qipt);
+		Vector3 axe = Vector3.Cross (qipe, qipt);
+		joint.Rotate (axe , alpha , Space.World);
+		if (joint.parent != null && !joint.Equals (start) && i < squelette.Length-1)
 		{
-			//Transform joint = maillonJoint.transform;
-			Vector3 qipe = new Vector3 (effector.position.x - joint.position.x, effector.position.y - joint.position.y, effector.position.z - joint.position.z);
-			Vector3 qipt = new Vector3 (start.position.x - joint.position.x, start.position.y + 500 - joint.position.y, start.position.z + 200 - joint.position.z);
-			float alpha = ComputeAngle3D (qipe, qipt);
-			Vector3 axe = Vector3.Cross (qipe, qipt);
-			joint.Rotate (axe, alpha, Space.World);
-
-			//if (maillonJoint.pere != null && !joint.Equals (start)) {
-			a = a + 1;
-					CCDStep3D (i[a], effector);
-			//}
+			i = i + 1;
+			CCDStep3D (squelette[i], effector, target, i);
+		}
+		else 
+		{
+			i = 1;
+			//if (y < maxY) target = new Vector3 (target.x, target.y + 1, target.z);
+			//if (z < maxZ) target = new Vector3 (target.x, target.y, target.z + 1);
+			//if (y < maxY && z < maxZ) CCDStep3D(squelette[1], squelette[0], target, i);
 		}
 	}
 }
