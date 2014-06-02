7552f6393837331ae99eb00a9d9ebe0088222fec
fboschet <boschet.francois@gmail.com>
2014-06-02 18:45:46 +0200

    Pour la doc Doxygen <3

---
+++ Assets/AdaptiveFont.cs
@@ -0,0 +1,51 @@
+/// \class AdaptiveFont
+/// Script pour modifier dynamiquement la taille de la police du bonton
+/// Ce script sert aussi à cherger les scènes lorsque le bouton est pressé.
+
+public class CCD3d : MonoBehaviour {
+	#region script_parameters	
+	
+	/// La taille de la police au repos
+	private int s_size_idle;
+	
+	/// La taille de la police lorsque le bouton est survolé par la souris
+	private int s_size_active;
+
+  /// L'identificateur du niveau a charger au clic
+	public string levelName;
+
+  /// La largeur actuelle de la fenêtre
+	private int screenRes;
+	#endregion script_parameters
+
+	// Use this for initialization
+	void Start () {
+	screenRes = Screen.width;
+		s_size_idle = (int)(screenRes/13.6);
+		s_size_active = s_size_idle+10;
+		this.guiText.fontSize = s_size_idle
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/// \brief Lorsque la souris arrive au dessus du bouton, 
+  /// la police passe a la taille s_size_active
+	void OnMouseEnter() {
+		this.guiText.fontSize = s_size_active;
+	}
+
+  /// \brief Lorsque la souris quitte le bouton, 
+  /// la police passe a la taille s_size_idle
+	void OnMouseExit() {
+		this.guiText.fontSize = s_size_idle;
+	}
+
+  /// \brief Lorsque l'utilisateur relache le clic, 
+  /// on charge le niveau "levelName"
+	void OnMouseUp() {
+		Application.LoadLevel(levelName);
+
+	}
+}
